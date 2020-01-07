ebb82d5e448311af5b71c0059886718bceda0204
davidchandra95 <david.chandra@tokopedia.com>
2020-01-07 11:37:51 +0700

    init

---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	//"github.com/novalagung/gubrak"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type M map[string]interface{}
+
+const (
+	MESSAGE_NEW_USER = "New User"
+	MESSAGE_CHAT     = "CHAT"
+	MESSAGE_LEAVE    = "Leave"
+)
+
+var connections = make([]*WebSocketConnection, 0)
+
+// Storing payload from front-end
+type SocketPayload struct {
+	Message string
+}
+
+// For broadcasting message to all connected users
+type SocketResponse struct {
+	From    string
+	Type    string
+	Message string
+}
+
+// Store client data
+type WebSocketConnection struct {
+	*websocket.Conn
+	Username string
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
+			return
+		}
+
+		_, _ = fmt.Fprintf(w, "%s", content)
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		currentGorillaConn, err := upgrader.Upgrade(w, r, w.Header())
+		if err != nil {
+			http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		}
+
+		username := r.URL.Query().Get("username")
+		currentConn := WebSocketConnection{Conn: currentGorillaConn, Username: username}
+		connections = append(connections, &currentConn)
+
+		go handleIO(&currentConn, connections)
+	})
+
+	fmt.Println("Server starting at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleIO(currentConn *WebSocketConnection, connections []*WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	broadcastMessage(currentConn, MESSAGE_NEW_USER, "")
+
+	for {
+		payload := SocketPayload{}
+		err := currentConn.ReadJSON(&payload)
+		if err != nil {
+			if strings.Contains(err.Error(), "websocket: close") {
+				broadcastMessage(currentConn, MESSAGE_LEAVE, "")
+				ejectConnection(currentConn)
+
+				return
+			}
+
+			log.Println("ERROR", err.Error())
+			continue
+		}
+
+		broadcastMessage(currentConn, MESSAGE_CHAT, payload.Message)
+	}
+}
+
+func ejectConnection(currentConn *WebSocketConnection) {
+	// remove conn from list of connected
+	for i := len(connections) - 1; i >= 0; i-- {
+		connection := connections[i]
+		// Condition to decide if current element has to be deleted:
+		if connection == currentConn {
+			connections = append(connections[:i],
+				connections[i+1:]...)
+		}
+	}
+}
+
+func broadcastMessage(currentConn *WebSocketConnection, kind, message string) {
+	for _, eachConn := range connections {
+		if eachConn == currentConn {
+			continue
+		}
+
+		_ = eachConn.WriteJSON(SocketResponse{
+			From:    currentConn.Username,
+			Type:    kind,
+			Message: message,
+		})
+	}
+}
